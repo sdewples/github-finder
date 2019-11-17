60eb022b289484756b6b8d2c7b27181b2c27b504
daniel <sdewples@gmail.com>
2019-11-17 00:28:27 -0500

    prototype github user finder sans api

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 'id',
+      login: 'sdewples',
+      avatar_url: 'https://avatars3.githubusercontent.com/u/56274860?s=460&v=4',
+      html_url: 'https://github.com/sdewples'
+    };
+  }
+  render() {
+    const { login, avatar_url, html_url } = this.state;
+    return (
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          alt=''
+          className='round-img'
+          style={{ width: '60px' }}
+        />
+        <h3>{login}</h3>
+
+        <div>
+          <a href={html_url} className='btn btn-dark btn-sm my-1'>
+            More
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserItem;
